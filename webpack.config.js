d07f870b0f87b8bf17794a546058b63b24817a4c
StevenMcD <StevenMcD@gmail.com>
2016-01-23 13:31:45 +0200

    Added main.js and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var config = {
+  entry : ['./src/main.js'],
+  output : {
+    path : path.resolve(__dirname, 'dist'),
+    filename : 'bundle.js'
+  },
+  modules : {
+    loaders : [
+      {
+        test : /\.js$/,
+        loaders : ['babel'],
+        exclude : /node_modules/
+      }
+    ]
+  }
+}
+
+module.exports = config;
